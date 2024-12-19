23b9bd651114c5c2bdf2e332664b526755be0fe4
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-19 16:44:29 +0100

    Try

--- task2.py
+++ task2.py
@@ -39,16 +39,23 @@ if __name__ == '__main__':
             # Look how many times the cycle
             steps = 0
             # while guard going
+            visited_positions = set()
             while True:
                 # Check if row and column are within bounds
                 if row < 0 or row >= len(lab) or column < 0 or column >= len(lab[0]):
                     break  # Exit the loop
                 steps += 1
-                if steps > 15*len_matrix:
+                # if steps > 15*len_matrix:
+                #     guard_is_cycled += 1
+                #     break
+                if (row, column) in visited_positions:  # Check if current position is visited
                     guard_is_cycled += 1
                     break
+                visited_positions.add((row, column))  # Add current position to visited set
                 lab, path, row, column, up, right, down, left =  \
                     fnc.check_all_directions(lab, path, row, column, up, right, down, left)
+                if len(visited_positions) >= len(lab) * len(lab[0]):
+                    break  # Guard hasn't found a loop after visiting all positions
             # if steps > 15*len_matrix:
             #     break
 
